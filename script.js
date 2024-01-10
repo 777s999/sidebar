352654eaec9d634c2960193e4daeea8630a874d6
Subrata Rout <subrataa916@gmail.com>
2024-01-11 00:06:41 +0530

    adding js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict';
+const sidebar = document.getElementById('sidebar');
+const menubtn = document.getElementById('menu');
+const content = document.getElementById('content');
+
+menubtn.addEventListener('click', () => {
+  sidebar.classList.toggle('active');
+  content.classList.toggle('active');
+});
